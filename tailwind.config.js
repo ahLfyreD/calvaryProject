d46148a7ce0cff3054e64fb207aacc3bbe5655a4
Alfred <fredileader@gmail.com>
2024-08-22 15:59:25 +0100

    image display

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        homeImage: 'url(./src/assets/images/calvaryImage1.png)',
+        homeImage: 'url(https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/452918840_456319823931547_1541801858347856689_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeF0J_d05lwQqv11zyZOhO1_bjsWRDCEhZFuOxZEMISFkeIA5CvyE9ms9lTRxVXEOhNfOo_M3dC5rgQ8qNQGXT0_&_nc_ohc=0PDCVMI79koQ7kNvgFWT5ls&_nc_ht=scontent-los2-1.xx&oh=00_AYDc_gFxHFZ9Kv_cke8mWdSiZS7DZgElpkj8wdpZbDNj2w&oe=66CD1A08)',
         pastorsImage: 'url(./src/assets/images/pastors.jpg)',
         pastorImage: 'url(./src/assets/images/PastorPapa.jpg)',
         logo: 'url(./src/assets/images/calvaryLogo.png)',
